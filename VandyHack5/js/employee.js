1a74b4f6d87f0f878bc11a4013380b601b424156
Amber Gupta <33590295+agupta1897@users.noreply.github.com>
2018-11-03 01:52:23 -0500

    Employee add function

--- VandyHack5/js/employee.js
+++ VandyHack5/js/employee.js
@@ -17,14 +17,24 @@ $(document).ready(function(){
         var employee_name = $('#employee_name').val();
         var phone_number = $('#phone_number').val();
         var email_address = $('#email_address').val();
-        var SSN = $('#SSN').val();
+        var social_security_number = $('#social_security_number').val();
       
-        var random = 100 
-        firebaseRef.child(random).child("employee_name").set(employee_name);
-        firebaseRef.child(random).child("phone_number").set(phone_number);
-        firebaseRef.child(random).child("email_address").set(email_address);
-        firebaseRef.child(random).child("SSN").set(SSN);
+       addEmployee();
 
     });
 
 });
+
+function addEmployee()
+{
+
+    var employee_name = $('#employee_name').val();
+        var phone_number = $('#phone_number').val();
+        var email_address = $('#email_address').val();
+        var social_security_number = $('#social_security_number').val();
+    var random = "employee";
+    firebaseRef.child(random).child("employee_name").set(employee_name);
+    firebaseRef.child(random).child("phone_number").set(phone_number);
+    firebaseRef.child(random).child("email_address").set(email_address);
+    firebaseRef.child(random).child("social_security_number").set(social_security_number);
+}
